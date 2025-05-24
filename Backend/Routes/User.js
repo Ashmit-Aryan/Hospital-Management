ca766d6c8ce565adff4b3f847bb7e00021b98cf3
Ashmit-Aryan <bhattashmit19@outlook.com>
2025-05-24 23:28:11 +0530

    User Model Created and User Route Added

---
+++ Backend/Routes/User.js
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+const { default: mongoose } = require("mongoose");
+const User = require("../Models/user");
+
+
--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,13 +1,14 @@
 import mongoose, { mongo } from "mongoose"
 
 const user = new mongoose.Schema({
-    name: {type:String,require:true},
-    email: {type:String,require:true,unique:true},
-    password: {type:String,require:true},
-    role: {type:String,require:true},
-    phone: {type:String,require:true},
-    address: {type:String,require:true},
-    createdAt: {type:Date,require:true,default:Date.now}
+    name: {type:String,required:true,min:6,max:255},
+    email: {type:String,required:true,unique:true,min:6,max:255},
+    password: {type:String,required:true,min:8,max:1024},
+    role: {type:String,required:true,min:6,max:255},
+    phone: {type:String,required:true,min:10,max:10},
+    address: {type:String,required:true,min:6,max:255},
+    createdBy:{type:String,required:true,min:6,max:255},
+    createdAt: {type:Date,required:true,default:Date.now}
 })
 
 module.exports = mongoose.model("User",user);
