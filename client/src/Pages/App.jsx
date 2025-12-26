25d95d83fb3a94e7726e3961df611bf61b3f4e1d
Ashmit-Aryan <bhattashmit19@outlook.com>
2025-12-26 23:29:57 +0530

    Added Vercel Analytics and Changed Head Title

--- client/src/Pages/App.jsx
+++ client/src/Pages/App.jsx
@@ -1,11 +1,12 @@
 import '../css/App.css'
 import ComingSoon from './ComingSoon'
-
+import { Analytics } from "@vercel/analytics/next"
 function App() {
 
   return (
     <>
      <ComingSoon />
+      <Analytics />
     </>
   )
 }
