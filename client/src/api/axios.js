c47cbbcb9b01702d2e626954805c709361d93f4e
Ashmit Aryan <ashmitbhatt19@gmail.com>
2026-01-26 23:26:48 +0530

    Base URl Updated in Frontend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://hospital-management-api-kohl.vercel.app/api",
   headers: { "Content-Type": "application/json" },
 });
 //https://hospital-management-api-kohl.vercel.app/
